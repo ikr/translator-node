f3149ca5884abdc54d9c4727224afcbde7254442
Ivan Krechetov <ivan.krechetov@gmail.com>
2014-01-23 15:48:03 +0100

    Extract test data()

--- tests/adaptI18nBrowserJs.test.js
+++ tests/adaptI18nBrowserJs.test.js
@@ -7,6 +7,24 @@ describe('adaptI18nBrowserJs()', function () {
         enUsBrowserJs = fs.readFileSync(__dirname + '/data/en_us_browser_js', 'utf8'),
 
         adaptI18nBrowserJs = require('../src/adaptI18nBrowserJs'),
+
+        data = function () {
+            return {
+                NAME_PREFIX: 'Mr.',
+                LAST_NAME: 'Krechetov',
+                ROOM_COUNT: 2,
+                RESERVATION_ID: 'R1545',
+                CHECK_IN_DATE: '22.01.2014',
+                CHECK_OUT_DATE: '23.01.2014',
+                HOTEL_NAME: 'Hotel Wartmann',
+                HOTEL_STARS: '***',
+                HOTEL_ADDRESS: 'Gleis NaN',
+                HOTEL_ZIP: '8400',
+                HOTEL_CITY: 'Winterthur',
+                CANCELLATION_URL: 'http://example.com/12345'
+            };
+        },
+
         i18n;
 
     beforeEach(function () {
@@ -19,13 +37,7 @@ describe('adaptI18nBrowserJs()', function () {
 
     it('results in namespaced and functional i18n functions', function () {
         assert.strictEqual(
-            i18n['email/booking/confirmation'].subject({
-                CHECK_IN_DATE: '22.01.2014',
-                CHECK_OUT_DATE: '23.01.2014',
-                HOTEL_NAME: 'Hotel Wartmann',
-                HOTEL_CITY: 'Winterthur'
-            }),
-
+            i18n['email/booking/confirmation'].subject(data()),
             'Reservation from 22.01.2014 to 23.01.2014 in Hotel Wartmann, Winterthur'
         );
     });
